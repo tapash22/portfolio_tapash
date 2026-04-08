a54b9abb903d734fb10b462b43d7adcfeed5b624
tapash22 <tapasp263@gmail.com>
2026-04-08 22:03:26 +0600

    sidebar with active added underline also

--- src/componants/sidebar/SIdeBar.tsx
+++ src/componants/sidebar/SIdeBar.tsx
@@ -4,8 +4,10 @@ import { routeConfig } from "../../routes/routeConfig";
 export function SideBar() {
   return (
     <div className="w-full h-screen flex justify-start items-center p-5 bg-(--sidebar)">
-      <div className="flex flex-col space-y-5">
-        <h1 className="text-3xl text-center italic text-(--foreground)">T</h1>
+      <div className="flex flex-col ">
+        <h1 className="text-4xl font-bold text-start px-5 italic text-(--foreground)">
+          T
+        </h1>
 
         <ul className="flex flex-col space-y-3 w-full p-5">
           {routeConfig.map((route, i) => {
@@ -15,7 +17,13 @@ export function SideBar() {
               <NavLink
                 key={i}
                 to={to}
-                className="link text-(--muted) hover:text-(--foreground) transition-colors"
+                className={({ isActive }) =>
+                  `link transition-colors ${
+                    isActive
+                      ? "text-(--foreground) active-link"
+                      : "text-(--muted) hover:text-(--foreground)"
+                  }`
+                }
               >
                 {route.title}
               </NavLink>
