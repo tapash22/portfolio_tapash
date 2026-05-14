5c47b5846acc02a5a51bfda8287c0e3701cd711d
tapash22 <tapasp263@gmail.com>
2026-05-15 04:27:27 +0600

    update social media link

--- src/storage/data/social-media-links.ts
+++ src/storage/data/social-media-links.ts
@@ -1,29 +1,29 @@
-import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import type { SocialMediaItems } from "../type/data-type";
 
 export const socialMediaLinkList: SocialMediaItems = [
   {
     id: 1,
     title: "Facebook",
-    link: "https://facebook.com/your-profile",
+    link: "https://web.facebook.com/tapas.paul.54738/",
     icon: FaFacebook,
   },
   {
     id: 2,
     title: "Twitter",
-    link: "https://twitter.com/your-profile",
+    link: "https://x.com/tapasp263",
     icon: FaTwitter,
   },
   {
     id: 3,
     title: "GitHub",
-    link: "https://github.com/your-username",
+    link: "https://github.com/tapash22",
     icon: FaGithub,
   },
   {
     id: 4,
     title: "LinkedIn",
-    link: "https://linkedin.com/in/your-profile",
+    link: "https://www.linkedin.com/in/tapash-paul-267896151/",
     icon: FaLinkedin,
   },
 ];
